7ec757553753b3767b590ba1d4071ef7ce734a6c
kimizhang <kimizhang@swireproperties.com>
2019-09-19 08:38:32 +0800

    括号匹配算法解法一

---
+++ stack/src/com/kimizhang/example/Solution.java
@@ -0,0 +1,43 @@
+package com.kimizhang.example;
+
+/**
+ * 括号匹配算法解法一
+ * 第一步：初始化一个长度为输入参数String的长度的数组，作为我们的栈.
+ * 第二步：初始化栈顶为 -1.
+ * 第三步：遍历我们的栈，如果元素为'('、'{'、'['入栈；如果为右括号，判断栈顶元素是否为与之匹配的左括号，是的话出栈，不是false.
+ * 第四步：最后栈顶为为 -1，表示栈中元素全部出栈，返回 true
+ * @author Kimi
+ * @date 2019/9/18
+ */
+public class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            if (currentChar == '(' || currentChar == '{' || currentChar == '['){
+                stack[++top] = currentChar;
+            }else if (currentChar == ')'){
+                if (top >= 0 && stack[top] == '('){
+                    top--;
+                }else {
+                    return false;
+                }
+            }else if (currentChar == '}'){
+                if (top >= 0 && stack[top] == '{'){
+                    top--;
+                }else {
+                    return false;
+                }
+            }else if (currentChar == ']'){
+                if (top >= 0 && stack[top] == '['){
+                    top--;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return top == -1;
+    }
+}
